173e415d7066b3569e975dfb9f93188352355fc7
Björn Guth <bjoern.guth@rwth-aachen.de>
2014-09-29 10:53:07 +0200

    added a first admin menu
    usless but it works

--- reso_db.php
+++ reso_db.php
@@ -20,7 +20,29 @@ if ( !function_exists( 'add_action' ) ) {
 #require_once( 'RESODB_PLUGIN_DIR' . 'test.php' );
 
 add_action( 'plugins_loaded', array( 'PageTemplater', 'get_instance' ) );
+add_action( 'admin_menu', 'my_plugin_menu' );
 
+/*
+ * First baby steps in adding a administration menu
+ */
+
+function my_plugin_menu() {
+	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+}
+
+function my_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+
+/*
+ * A class to add templates 
+ */
 
 class PageTemplater {
 
